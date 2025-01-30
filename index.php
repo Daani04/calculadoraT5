820d9a1963bb831e607529d51a8b5631429d9457
Dani Arastell <daniarastell@Danis-MacBook-Pro.local>
2025-01-30 13:59:57 +0100

    Corrigiendo versiones

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $resultado = ($num1 >= 0) ? $calc->raiz($num1) : "Error: No se puede calcular raíz de número negativo";
             break;
         default:
-            $resultado = "Operación no válida.";
+            $resultado = "Operación no válidaa.";
     }
 }
 ?>
